eecce9af76df1ed1b9d56c1b8b244756ed5d9b54
Cillian <cillsm80@gmail.com>
2024-03-03 19:51:35 +0000

    Lab4Part2 Done

--- Lab4/src/OutputInternationalInformation.java
+++ Lab4/src/OutputInternationalInformation.java
@@ -1,36 +1,53 @@
 import java.text.DateFormatSymbols;
+import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.time.chrono.IsoChronology;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.FormatStyle;
 import java.util.Arrays;
+import java.util.Currency;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class OutputInternationalInformation {
     public static void main(String[] args) {
         Locale FR = new Locale("fr", "FR");
-        Locale EN = new Locale("en", "EN");
+        Locale EN = new Locale("en", "GB");
         Locale JP = new Locale("ja", "JP");
 
-        displayLocalizedInformation(EN);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please choose a locale: \n 1. FR\n 2. EN\n 3. JP\n Enter a number");
+        int selection = sc.nextInt();
+        switch (selection) {
+            case 1 -> displayLocalizedInformation(FR);
+            case 2 -> displayLocalizedInformation(EN);
+            case 3 -> displayLocalizedInformation(JP);
+
+        }
     }
 
     public static void displayLocalizedInformation(Locale locale) {
-        System.out.printf("Localisation = %S \nDays of week = %S \nMonths = %S",
-                locale.toString(), getDays(locale, "days"), getDays(locale, "months"));
-
-        System.out.println("********************************");
-        System.out.println();
+        System.out.printf("Localisation = %S \n%S \n%S", locale.toString(), getDayData(locale), getCurrencyData(locale));
     }
 
-    private static String getDays(Locale locale, String datasetWanted) {
+    private static String getDayData(Locale locale) {
         DateFormatSymbols dateFormatSymbols = new DateFormatSymbols(locale);
-        DateTimeFormatter dateTime = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+        String datePattern = DateTimeFormatterBuilder.getLocalizedDateTimePattern(FormatStyle.SHORT, null, IsoChronology.INSTANCE, locale);
+        LocalDateTime time = LocalDateTime.now();
 
+        return "Days = " + Arrays.toString(dateFormatSymbols.getWeekdays()) +
+                "\nMonths + " + Arrays.toString(dateFormatSymbols.getMonths()) +
+                "\nDate = " + time.format(DateTimeFormatter.ofPattern(datePattern)) +
+                "\nEras = " + Arrays.toString(dateFormatSymbols.getEras());
+    }
 
-        return switch (datasetWanted) {
-            case "days" -> Arrays.toString(dateFormatSymbols.getWeekdays());
-            case "months" -> Arrays.toString(dateFormatSymbols.getMonths());
-            case "dates" -> dateTime.toString();
-            default -> null;
-        };
+    private static String getCurrencyData(Locale locale) {
+        NumberFormat numFormat = NumberFormat.getCurrencyInstance(locale);
+        Currency currency = Currency.getInstance(locale);
+        numFormat.setCurrency(currency);
+        String formattedCurrency = numFormat.format(10000.50);
+        return "Currency = " + formattedCurrency;
     }
 }
