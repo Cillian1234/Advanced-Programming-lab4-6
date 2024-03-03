0e8d1b8ab13456ce8d6420cc37a1c4569dec85f6
Cillian <cillsm80@gmail.com>
2024-03-03 05:30:34 +0000

    Update OutputInternationalInformation.java

--- Lab4/src/OutputInternationalInformation.java
+++ Lab4/src/OutputInternationalInformation.java
@@ -1,12 +1,36 @@
+import java.text.DateFormatSymbols;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class OutputInternationalInformation {
     public static void main(String[] args) {
+        Locale FR = new Locale("fr", "FR");
+        Locale EN = new Locale("en", "EN");
+        Locale JP = new Locale("ja", "JP");
 
+        displayLocalizedInformation(EN);
     }
 
     public static void displayLocalizedInformation(Locale locale) {
-        Locale FR = new Locale("fr", "FR");
-        Locale PH = new Locale("ph", "PH");
+        System.out.printf("Localisation = %S \nDays of week = %S \nMonths = %S",
+                locale.toString(), getDays(locale, "days"), getDays(locale, "months"));
+
+        System.out.println("********************************");
+        System.out.println();
+    }
+
+    private static String getDays(Locale locale, String datasetWanted) {
+        DateFormatSymbols dateFormatSymbols = new DateFormatSymbols(locale);
+        DateTimeFormatter dateTime = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+
+
+        return switch (datasetWanted) {
+            case "days" -> Arrays.toString(dateFormatSymbols.getWeekdays());
+            case "months" -> Arrays.toString(dateFormatSymbols.getMonths());
+            case "dates" -> dateTime.toString();
+            default -> null;
+        };
     }
 }
